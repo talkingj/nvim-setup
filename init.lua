de9fd9855d4ae50d0c57c97b2c78483bcc069e0e
James <j.talkington@student.uq.edu.au>
2023-06-18 12:54:09 +1000

    Added TreeSitter support

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
+
+
 require('packer').startup(function()
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
@@ -28,6 +30,12 @@ require('packer').startup(function()
     }
   use 'nvim-treesitter/playground'
   use { "catppuccin/nvim", as = "catppuccin" }
+  use {
+  'nvim-tree/nvim-tree.lua',
+  requires = {
+    'nvim-tree/nvim-web-devicons', -- optional
+  },
+}
 end)
 
 local g = vim.g
@@ -213,3 +221,27 @@ require("catppuccin").setup({
 
 vim.cmd.colorscheme "catppuccin"
 cmd("autocmd VimEnter * COQnow")
+
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
+-- empty setup using defaults
+require("nvim-tree").setup()
+
+-- OR setup with some options
+require("nvim-tree").setup({
+  sort_by = "case_sensitive",
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
