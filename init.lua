967d7421fd58244c1e5b57c268beb321d84a163a
James <j.talkington@uqconnect.edu.au>
2023-08-14 10:34:04 +1000

    Set up Mason

--- init.lua
+++ init.lua
@@ -30,6 +30,11 @@ require('packer').startup(function()
     }
   use 'nvim-treesitter/playground'
   use { "catppuccin/nvim", as = "catppuccin" }
+  use { "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig", 
+     }
+
   use {
   'nvim-tree/nvim-tree.lua',
   requires = {
@@ -262,6 +267,38 @@ require('arduino').setup {
     extra_opts = { ... }
 }
 
-require 'lspconfig' ['arduino_language_server'].setup {
-    on_new_config = require('arduino').on_new_config,
+require("mason").setup({
+    ui = {
+        icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+        }
+    }
+})
+
+require 'arduino'.setup({
+    clangd = require 'mason-core.path'.bin_prefix 'clangd',
+    -- other settings
+})
+
+local DEFAULT_SETTINGS = {
+    -- A list of servers to automatically install if they're not already installed. Example: { "rust_analyzer@nightly", "lua_ls" }
+    -- This setting has no relation with the `automatic_installation` setting.
+    ---@type string[]
+    ensure_installed = {},
+
+    -- Whether servers that are set up (via lspconfig) should be automatically installed if they're not already installed.
+    -- This setting has no relation with the `ensure_installed` setting.
+    -- Can either be:
+    --   - false: Servers are not automatically installed.
+    --   - true: All servers set up via lspconfig are automatically installed.
+    --   - { exclude: string[] }: All servers set up via lspconfig, except the ones provided in the list, are automatically installed.
+    --       Example: automatic_installation = { exclude = { "rust_analyzer", "solargraph" } }
+    ---@type boolean
+    automatic_installation = false,
+
+    -- See `:h mason-lspconfig.setup_handlers()`
+    ---@type table<string, fun(server_name: string)>?
+    handlers = nil,
 }
